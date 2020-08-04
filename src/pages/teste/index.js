b078b37e281053dab9f40b95f4bfd9043baacbfe
iagoAguiar <iagoaguiar202@gmail.com>
2020-08-04 16:57:25 -0300

    adicionando  imagem

--- src/pages/teste/index.js
+++ src/pages/teste/index.js
@@ -4,11 +4,12 @@ import React from 'react';
 function pagina2(){
     return(
         <div>
+            <img src={"https://ubistatic19-a.akamaihd.net/ubicomstatic/pt-br/global/search-thumbnail/fc_newdawn-ubicom_gamepage-search_thumbnail-750x422_mobile_341165.jpg"}></img>
             <img src={"https://i.pinimg.com/originals/2d/d2/a7/2dd2a74b7cec2a81aa3c624f7b43a087.jpg"}></img>
             <p>teste</p>
             <p>teste2</p>
             <p>teste23</p>
-            <p>teste23</p>
+            <p>test88888</p>
         </div>
         )
 }
