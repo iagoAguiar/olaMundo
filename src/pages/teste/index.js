d0781c30bd0cbf307418b353d881d8d8e0939d6b
iagoAguiar <iagoaguiar202@gmail.com>
2020-08-04 16:47:00 -0300

    corrigindo pagina 2

--- src/pages/teste/index.js
+++ src/pages/teste/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 function pagina2(){
     return(
-        <img src={"https://i.pinimg.com/originals/2d/d2/a7/2dd2a74b7cec2a81aa3c624f7b43a087.jpg"}></img>
-        <p>teste</p>
-        <p>teste2</p>
-        <p>teste23</p>
-        <p>teste23</p>
+        <div>
+            <img src={"https://i.pinimg.com/originals/2d/d2/a7/2dd2a74b7cec2a81aa3c624f7b43a087.jpg"}></img>
+            <p>teste</p>
+            <p>teste2</p>
+            <p>teste23</p>
+            <p>teste23</p>
+        </div>
         )
 }
 
